599f384eba1ab3ac95da5ce98cbe60a46f109557
clement <clement.roussel@onf.fr>
2026-03-02 13:57:24 +0100

    Update Accueil.py

--- Accueil.py
+++ Accueil.py
@@ -2,4 +2,6 @@ import streamlit as st
 
 st.title("Calculs hydrauliques sur section irrégulière")
 
-st.text("toto")
+st.write("""
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed venenatis turpis. Nam feugiat, lacus sed consectetur egestas, dolor ante luctus neque, ut tincidunt sapien nunc sit amet arcu. Phasellus viverra, diam ut euismod bibendum, enim nisl facilisis nisi, id aliquam sapien mi ut tortor. Praesent sollicitudin purus non elit egestas lacinia. Nulla facilisi. Maecenas in tincidunt justo. Fusce id mauris sem. Curabitur euismod urna enim, sit amet pretium velit tristique ac. Nam interdum lacus commodo libero molestie pharetra. Fusce sollicitudin odio erat, sit amet pharetra diam molestie non. Morbi feugiat sollicitudin orci et ultrices. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed aliquam gravida ipsum sed congue. 
+""")
