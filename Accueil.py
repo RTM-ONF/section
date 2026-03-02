b8251684e8df748c805577df29ccd722fea3e800
clement <clement.roussel@onf.fr>
2026-03-02 15:12:45 +0100

    updates

--- Accueil.py
+++ Accueil.py
@@ -2,6 +2,12 @@ import streamlit as st
 
 st.title("Calculs hydrauliques sur section irrégulière")
 
-st.write("""
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed venenatis turpis. Nam feugiat, lacus sed consectetur egestas, dolor ante luctus neque, ut tincidunt sapien nunc sit amet arcu. Phasellus viverra, diam ut euismod bibendum, enim nisl facilisis nisi, id aliquam sapien mi ut tortor. Praesent sollicitudin purus non elit egestas lacinia. Nulla facilisi. Maecenas in tincidunt justo. Fusce id mauris sem. Curabitur euismod urna enim, sit amet pretium velit tristique ac. Nam interdum lacus commodo libero molestie pharetra. Fusce sollicitudin odio erat, sit amet pharetra diam molestie non. Morbi feugiat sollicitudin orci et ultrices. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed aliquam gravida ipsum sed congue. 
-""")
+"""
+Cette feuille de calcul en ligne est dédiée aux calculs hydrauliques pour les sections irrégulières. 
+
+Grâce à une interface simple, vous pouvez importer la géométrie de votre profil transversal et obtenir instantanément des grandeurs clés comme l’aire, le périmètre mouillé ou le rayon hydraulique.
+
+Cet outil est idéal pour les ingénieurs, les étudiants et tous ceux qui souhaitent explorer différentes configurations de canal ou de lit de rivière en quelques clics.
+
+En quelques secondes, transformez la géométrie de votre section en résultats concrets et comparez différents scénarios pour optimiser vos projets hydrauliques.
+"""
--- pages/1_Importer_une_section.py
+++ pages/1_Importer_une_section.py
@@ -1,3 +1,48 @@
+import pandas as pd
 import streamlit as st
 
 st.title("Importer une section")
+
+uploaded_file = st.file_uploader(
+    "Téléverser le fichier contenant la géométrie de la section")
+
+col1, col2 = st.columns(2)
+
+delimiters = {"\t" : "tabulation" ,
+              " " : "espace",
+              ";" : "point-virgule",
+              "," : "virgule"}
+
+with col1:
+    delimiter = st.selectbox("Délimiteur",
+                             options=delimiters.keys(),
+                             format_func=lambda x : delimiters[x])
+
+    distance_field = st.text_input("Nom du champ des distances",
+                                   "X")
+
+separators = {"." : "point",
+              "," : "virgule"}
+
+with col2:
+    separator = st.selectbox("Séparateur décimal",
+                             options=separators.keys(),
+                             format_func=lambda x : separators[x])
+
+    altitude_field = st.text_input("Nom du champ des altitudes",
+                                   "Z")
+
+if uploaded_file is not None:
+    if st.button("Importer la section !"):
+        try:
+            # Lire le fichier CSV avec pandas
+            df = pd.read_csv(uploaded_file,
+                             delimiter=delimiter,
+                             decimal=separator)
+            
+            # Affichage du dataframe
+            st.success("Géométrie chargée avec succès !")
+            st.write(f"Nom du fichier téléversé : {uploaded_file.name}")
+            st.dataframe(df)
+        except Exception as e:
+            st.error(f"Erreur lors du chargement : {e}")
