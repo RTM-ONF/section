9777181cb22ea32691fb85fbaa716bf1d8425106
clement <clement.roussel@onf.fr>
2026-03-08 09:46:06 +0100

    Update Accueil.py

--- Accueil.py
+++ Accueil.py
@@ -3,28 +3,28 @@ import streamlit as st
 st.title("Calculs hydrauliques sur section irrégulière")
 
 """
-Cet outil a été conçu pour faciliter l’exploration, la visualisation et le calcul des principales caractéristiques géométriques et hydrauliques d’une **section irrégulière**.
+Cet outil a été conçu pour faciliter la visualisation et le calcul des principales caractéristiques géométriques et hydrauliques d’une **section irrégulière**.
 
 Pour vous accompagner étape par étape, l’application se compose de plusieurs onglets :
 
-### 1. Importer une section
+#### 1. Importer une section
 
 Importez votre fichier décrivant la section.
 
-### 2. Coordonnées et statistiques basiques
+#### 2. Coordonnées et statistiques
 
-Consultez les valeurs brutes de la géométrie ainsi que leurs statistiques essentielles.
+Consultez les valeurs brutes de la géométrie ainsi que les principales statistiques.
 
-### 3. Visualiser la section
+#### 3. Visualiser la section
 
 Explorez votre section sous forme de graphique interactif.
 
-### 4. Propriétés géométriques
+#### 4. Propriétés géométriques
 
 Renseignez une hauteur d’eau pour afficher automatiquement le niveau d’eau sur la section et
-les caractéristiques géométriques clés.
+les principales caractéristiques géométriques.
 
-### 5. Relation hauteur débit
+### 5. Courbes débit vs hauteur
 
 Générez les courbes de débit en fonction de la hauteur d’eau selon plusieurs modèles hydrauliques.
 """
